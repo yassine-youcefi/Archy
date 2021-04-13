4229822a302f061bc2052894ccb0e8394b7d60e6
yani27 <yanilacamora@gmail.com>
2021-04-13 12:35:04 +0100

    update

--- archy/shape.py
+++ archy/shape.py
@@ -15,39 +15,60 @@ class Point():
             return distance
 
         except ValueError as r:
-            print(r)
+            print(r.__class__() + r)
 
 
 class Rectangle():
 
     '''
-    init
-    long: longeur
-    large: large
+    init *args :
+        long: longeur
+        large: large
+
+    init **kwargs :
+        points : a , b , c , d 
     '''
 
-    def __init__(self, long, large):
-        self.long = long
-        self.large = large
+    def __init__(self, *args, **kwargs):
 
-    def __repr__(self):
-        return 'long : {} , large : {}'.format(self.long, self.large)
+        try:
+            if len(args) > 0 and len(kwargs) > 0:
+                raise ValueError(
+                    'the class can take one type of arguments args or kwargs')
+
+            for key, value in kwargs.items():
+                print("value =", value)
+                if not isinstance(value, Point):
+                    print('OKKK')
+                #     raise ValueError('all params must be points')
+                # self.a = kwargs['a']
+                # self.b = kwargs['b']
+                # self.c = kwargs['c']
+                # self.d = kwargs['d']
+        # self.long = args[0]
+        # self.large = args[1]
+
+        except ValueError as r:
+            print(r)
+
+    # def __repr__(self):
+    #     return 'long : {} , large : {}'.format(self.long, self.large)
 
     '''
     surface this method calculate a surface of the object
     '''
 
-    def surface(self):
-        surface = self.long*self.large
-        return surface
+    # def surface(self):
+    #     surface = self.long*self.large
+    #     return surface
 
-    def perimetre(self):
-        perimetre = ((2*self.large)+(2*self.long))
-        return perimetre
+    # def perimetre(self):
+    #     perimetre = ((2*self.large)+(2*self.long))
+    #     return perimetre
 
-    def diagonal(self):
-        diagonal = sqrt(pow(self.long, 2)+pow(self.large, 2))
-        return diagonal
+    # def diagonal(self):
+    #     diagonal = sqrt(pow(self.long, 2)+pow(self.large, 2))
+    #     return diagonal
 
 
 class SpaceRectangle():
@@ -61,3 +82,4 @@ class SpaceRectangle():
         return 'a : {} , b : {} , c : {} , d : {}'.format(self.a, self.b, self.c, self.d)
 
     # def surface(self):
+    #     if isinstance
--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 from archy import Rectangle, SpaceRectangle, Point
-g = SpaceRectangle(a=(1, 3), b=(3, 3), c=(3, 1), d=(1, 1))
-print(g)
+
 
 p = Point(5, 10)
 p2 = Point(5, 10)
-print(p.distance(p2))
+p3 = Point(5, 10)
+p4 = Point(5, 10)
+
+
+g = Rectangle(p, p2, p3, p4)

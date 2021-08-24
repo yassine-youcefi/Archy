c76a414f7408b33222a1e71d43ed2620f96949f0
yassine-youcefi <mohamed.youcefi.etu@univ-mosta.dz>
2021-08-24 22:05:09 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Aerospace Mathematics'
+LONG_DESCRIPTION = 'This is a  python package is created to facilitate calculations in the field of aerospace Mathematics Engineering in particular and mathematics geometry in general'
+
+# Setting up
+setup(
+    name="Archiy",
+    version=VERSION,
+    author="youcefi yassine",
+    author_email="mohamed.youcefi.etu@univ-mosta.dz",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
